b1880676d960a96a52a19378ebce151eabd16f39
amastis <37512516+amastis@users.noreply.github.com>
2021-05-12 12:45:36 -0400

    Multiple Links supported just "" in --link usage w/ space btwn links

--- Web-Youtube.py
+++ Web-Youtube.py
@@ -9,11 +9,10 @@ from pathlib import Path
 from time import sleep
 import pandas as pd
 import requests
+import isodate
 import json
 import math
 import sys
-import re
-
 
 
 # parts and item_id will have to be properly formmated
@@ -33,10 +32,13 @@ def getComments(d_type, vid_type, api_key, parts='part=snippet', parent='id'):
 	next_pg = ''
 	all_comments, comments_with_replies = [], []
 	json_data = []
-	while first_run: # or 'nextPageToken' in json_comments:
+	while first_run:  # or 'nextPageToken' in json_comments:
 		first_run = False
 		json_comments = YT_json(f'comment{d_type}s', parts, vid_type, api_key, next_pg)
-		json_data += json_comments['items']
+		if 'items' in json_comments:
+			json_data += json_comments['items']
+		else: 
+			print(json_comments)
 		if 'nextPageToken' in json_comments:
 			first_run = True
 			next_pg = json_comments['nextPageToken']
@@ -52,25 +54,29 @@ def getComments(d_type, vid_type, api_key, parts='part=snippet', parent='id'):
 					all_comments.append(comment)
 			elif 'replies' in item:
 				comments_with_replies.append(item['id']) 
-		comment = [item['id'], cData['textOriginal'], cData['authorDisplayName'], cData['authorChannelUrl'], cData['likeCount'], cData['publishedAt'], cData['updatedAt'], item[parent]]			all_comments.append(comment)
+		comment = [item['id'], cData['textOriginal'], cData['authorDisplayName'], cData['authorChannelUrl'], cData['likeCount'], cData['publishedAt'], cData['updatedAt'], item[parent]]
+		all_comments.append(comment)
 	return all_comments, comments_with_replies
 
 # youtube API to get more specific information about the video
-def youtubeAPI(video_id, api_key, data_dict, commentsOn):
+def youtubeAPI(video_id, api_key, data_dict, cmtOn, secOn):
 	json_parts = 'part=snippet,statistics,contentDetails,topicDetails'
 	json_response = YT_json('videos', json_parts, f'&id={video_id}', api_key)
 	allComments = []
 	data_dict['video_url'] = 'https://www.youtube.com/watch?v=' + video_id
 	# to deal with deleted and private videos in playlists (mainly)
-	if json_response['pageInfo']['totalResults'] != 0:
+	if int(json_response['pageInfo']['totalResults']) > 0:
 		statistics = json_response['items'][0]['statistics']
-		data_dict['views'] = statistics['viewCount']
+		if 'viewCount' in statistics:  # due to streaming of videos
+			data_dict['views'] = statistics['viewCount']
+		else:
+			data_dict['views'] = 0
 		data_dict['likes'] = statistics['likeCount'] if 'likeCount' in statistics else 0
 		data_dict['dislikes'] = statistics['dislikeCount'] if 'dislikeCount' in statistics else 0
 		data_dict['favorites'] = statistics['favoriteCount']
 		if 'commentCount' in statistics and int(statistics['commentCount']) > 0:
 			data_dict['comment_number'] = statistics['commentCount']
-			if commentsOn:  # cycle through the various comments on the video
+			if cmtOn:  # cycle through the various comments on the video
 				commentSetup = ['comment_id', 'comment', 'comment_author', 'comment_author_url', 'comment_likes', 'comment_published', 'comment_updated_time', 'parent_comment']
 				allComments.append(commentSetup)
 				temp_data, c_replies = getComments('Thread', f'videoId={video_id}',  api_key)
@@ -98,7 +104,12 @@ def youtubeAPI(video_id, api_key, data_dict, commentsOn):
 		if 'tags' in snippet:
 			data_dict['video_tags'] = snippet['tags']
 		data_dict['category_Id'] = snippet['categoryId']
-		data_dict['ISO_8601_duration'] = json_response['items'][0]['contentDetails']['duration']
+		video_duration = json_response['items'][0]['contentDetails']['duration']
+		if secOn:
+			dur = isodate.parse_duration(video_duration)
+			data_dict['seconds_duration'] = dur.total_seconds()
+		else:
+			data_dict['ISO_8601_duration'] = video_duration
 		if 'topicDetails' in json_response['items'][0]:
 			data_dict['video_topics'] = json_response['items'][0]['topicDetails']['topicCategories']
 
@@ -114,107 +125,112 @@ def getIDFromLink(yt_link, splitThis):
 
 if __name__ == "__main__":
 	# using getopt to get the commands for the program to run with
-	yt_link, api_key, commentsOn = getCommands()
+	yt_link, api_key, cmtOn, secOn = getCommands()
 	api_key = '&key=' + api_key
+	all_links = [x for x in yt_link.split(' ') if "youtube" in x]
 
-	title = ''
-	scroll_amnt = 50  # arbitrary number for homepage/search -- user insert number to override?
-	single_video = False
-	chrome_options = Options()
-	chrome_options.add_argument("--headless")
-	driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+	for yt_link in all_links: # for multiple links inserted
+		print(yt_link, '')
+		title = ''
+		scroll_amnt = 50  # arbitrary number for homepage/search -- user insert number to override?
+		single_video = False
+		chrome_options = Options()
+		chrome_options.add_argument("--headless")
+		driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
-	title_id = 'video-title'  # default is for channels
-	if any(x in yt_link for x in ['/user/', '/channel/', '/c/']):  # Channel
-		split_link = yt_link.split('/')
-		user_id = split_link[-2] if 'videos' in yt_link else split_link[-1]
-		user_or_channel = f'&forUsername={user_id}' if '/user/' in yt_link else '&id='
-		if '/user/' not in yt_link:
+		title_id = 'video-title'  # default is for channels
+		if any(x in yt_link for x in ['/user/', '/channel/', '/c/']):  # Channel
+			split_link = yt_link.split('/')
+			user_id = split_link[-2] if 'videos' in yt_link else split_link[-1]
+			user_or_channel = f'&forUsername={user_id}' if '/user/' in yt_link else '&id='
+			if '/user/' not in yt_link:
+				driver.get(yt_link)
+				channel_name = driver.find_element_by_xpath('/html/body/link[1]').get_attribute('href')
+				user_or_channel += getIDFromLink(channel_name, '/')
+			json_response = YT_json('channels', 'part=id,statistics', user_or_channel, api_key)
+			scroll_amnt = int(int(json_response['items'][0]['statistics']['videoCount']) / 30) + 1
+			driver.get(f'https://www.youtube.com/{split_link[3]}/{user_id}/videos')  # update channel
+			title = driver.find_element_by_xpath('//*[@id="text-container"]').text + '.csv'
+		elif 'list=' in yt_link:  # Playlists - if its a video in a playlist or the full playlist
+			playlist_id = getIDFromLink(yt_link, 'list=')
+			driver.get(f'https://www.youtube.com/playlist?list={playlist_id}')
+			title = driver.find_element_by_xpath('//*[@id="title"]/yt-formatted-string/a').text + '.csv'
+			vids = driver.find_element_by_xpath('//*[@id="stats"]/yt-formatted-string[1]/span[1]').text
+			scroll_amnt = math.ceil(int(vids) / 100) + 1
+		elif '?' not in yt_link:  # YouTube Homepage
+			title_id = 'video-title-link'
+			title = 'YouTube Homepage.csv'
 			driver.get(yt_link)
-			channel_name = driver.find_element_by_xpath('/html/body/link[1]').get_attribute('href')
-			user_or_channel += getIDFromLink(channel_name, '/')
-		json_response = YT_json('channels', 'part=id,statistics', user_or_channel, api_key)
-		scroll_amnt = int(int(json_response['items'][0]['statistics']['videoCount']) / 30) + 1
-		driver.get(f'https://www.youtube.com/{split_link[3]}/{user_id}/videos')  # update channel
-		title = driver.find_element_by_xpath('//*[@id="text-container"]').text + '.csv'
-	elif 'list=' in yt_link:  # Playlists - if its a video in a playlist or the full playlist
-		playlist_id = getIDFromLink(yt_link, 'list=')
-		driver.get(f'https://www.youtube.com/playlist?list={playlist_id}')
-		title = driver.find_element_by_xpath('//*[@id="title"]/yt-formatted-string/a').text + '.csv'
-		vids = driver.find_element_by_xpath('//*[@id="stats"]/yt-formatted-string[1]/span[1]').text
-		scroll_amnt = math.ceil(int(vids) / 100) + 1
-	elif '?' not in yt_link:  # YouTube Homepage
-		title_id = 'video-title-link'
-		title = 'YouTube Homepage.csv'
-		driver.get(yt_link)
-	elif 'v=' in yt_link:  # single YouTube video
-		scroll_amnt = 0
-		single_video = True
-		driver.get(yt_link)
-	elif 'search_query' in yt_link:  # if using search bar
-		driver.get(yt_link)
-		meta = yt_link.split('search_query=')[1]
-		if '&' in meta:  # for extra commands tacked on
-			meta = meta.split('&')[0]
-		title = meta.replace('+', ' ') + '.csv'
-	else:  # improper usage
-		print('The entered youtube link is incompatible with the program')
-		print('youtube link styles that work:')
-		print('\thttps://www.youtube.com/')
-		print('\thttps://www.youtube.com/results?search_query=valuetainment')
-		print('\thttps://www.youtube.com/watch?v=x9dgZQsjR6s')
-		print('\thttps://www.youtube.com/user/patrickbetdavid')
-		print('\thttps://www.youtube.com/playlist?list=PLFa0bDwXvBlDGFtce9u__1sBj6fgi21BE')
+		elif 'v=' in yt_link:  # single YouTube video
+			scroll_amnt = 0
+			single_video = True
+			driver.get(yt_link)
+		elif 'search_query' in yt_link:  # if using search bar
+			driver.get(yt_link)
+			meta = yt_link.split('search_query=')[1]
+			if '&' in meta:  # for extra commands tacked on
+				meta = meta.split('&')[0]
+			title = meta.replace('+', ' ') + '.csv'
+		else:  # improper usage
+			print('The entered youtube link is incompatible with the program')
+			print('youtube link styles that work:')
+			print('\thttps://www.youtube.com/')
+			print('\thttps://www.youtube.com/results?search_query=valuetainment')
+			print('\thttps://www.youtube.com/watch?v=x9dgZQsjR6s')
+			print('\thttps://www.youtube.com/user/patrickbetdavid')
+			print('\thttps://www.youtube.com/playlist?list=PLFa0bDwXvBlDGFtce9u__1sBj6fgi21BE')
+			driver.close()
+			sys.exit(-1)
+		# scroll to the bottom of the youtube page
+		for i in range(scroll_amnt):
+			printProgressBar(i + 1, scroll_amnt, 'Navigating Youtube:', length=50)
+			driver.find_element_by_tag_name('body').send_keys(Keys.END)
+			sleep(3)
+
+		html = driver.page_source
+		soup = BeautifulSoup(html, 'html.parser')
+		master_list = []
+		if 'list=' not in yt_link and not single_video:  # channel, homepage, and search
+			videos = soup.find_all('div', {'id': 'dismissible'})
+			for video in videos:
+				if 'ytd-shelf-renderer' in video['class'] or 'ytd-compact-promoted-item-renderer' in video['class'] or 'ytd-rich-shelf-renderer' in video['class']:
+					continue  # remove the outer most nested 'video'
+				data_dict = {}
+				# get title
+				data_dict['title'] = video.find('a', {'id': title_id}).text.replace('\n', '')
+				# get video url + use id for youtube API
+				vid_id = video.find('a', {'id': title_id})['href'].split('=')[1]
+				data_dict, data_dict['comments'] = youtubeAPI(vid_id, api_key, data_dict, cmtOn, secOn)
+				master_list.append(data_dict)
+				printProgressBar(videos.index(video) + 1, len(videos), 'Processing Videos:', length=50)
+		elif single_video:
+			data_dict = {}
+			data_dict['title'] = soup.find('meta', {'name': 'title'})['content']
+			title = data_dict['title'] + '.csv'
+			data_dict, temp_item = youtubeAPI(yt_link.split('=')[1], api_key, data_dict, cmtOn, secOn)
+			for i in range(1, len(temp_item)):  # splitting up comments to their own row
+				for j, elm in enumerate(temp_item[i]):
+					data_dict[temp_item[0][j]] = elm.replace('\r', '\\r') if type(elm) == str else elm
+				master_list.append(data_dict)
+				data_dict = {}
+		else:  # works for playlist
+			playlist_videos = soup.find_all('div', {'id': 'contents'})[0]
+			videos = playlist_videos.find_all('div', {'id': 'content'})
+			for vid in videos:
+				data_dict = {}
+				# get title
+				data_dict['title'] = vid.find('a', {'id': 'video-title'})['title']  # was a 'span'
+				# get video url + use id for youtube API
+				vid_id = getIDFromLink(vid.find('a', {'class': 'yt-simple-endpoint'})['href'], 'v=')
+				data_dict, data_dict['comments'] = youtubeAPI(vid_id, api_key, data_dict, cmtOn, secOn)
+				master_list.append(data_dict)
+				printProgressBar(videos.index(vid) + 1, len(videos), 'Processing Videos:', length=50)
+
+
+		print('')  # new line for downloaded file
 		driver.close()
-		sys.exit(-1)
-	# scroll to the bottom of the youtube page
-	for i in range(scroll_amnt):
-		printProgressBar(i + 1, scroll_amnt, 'Navigating Youtube:', length=50)
-		driver.find_element_by_tag_name('body').send_keys(Keys.END)
-		sleep(3)
-
-	html = driver.page_source
-	soup = BeautifulSoup(html, 'html.parser')
-	master_list = []
-	if 'list=' not in yt_link and not single_video:  # channel, homepage, and search
-		videos = soup.find_all('div', {'id': 'dismissable'})
-		for video in videos:
-			if 'ytd-shelf-renderer' in video['class'] or 'ytd-compact-promoted-item-renderer' in video['class'] or 'ytd-rich-shelf-renderer' in video['class']:
-				continue  # remove the outer most nested 'video'
-			data_dict = {}
-			# get title
-			data_dict['title'] = video.find('a', {'id': title_id}).text.replace('\n', '')
-			# get video url + use id for youtube API
-			video_id = video.find('a', {'id': title_id})['href'].split('=')[1]
-			data_dict, data_dict['comments'] = youtubeAPI(video_id, api_key, data_dict, commentsOn)
-			master_list.append(data_dict)
-			printProgressBar(videos.index(video) + 1, len(videos), 'Processing Videos:', length=50)
-	elif single_video:
-		data_dict = {}
-		data_dict['title'] = soup.find('meta', {'name': 'title'})['content']
-		title = data_dict['title'] + '.csv'
-		data_dict, temp_item = youtubeAPI(yt_link.split('=')[1], api_key, data_dict, commentsOn)
-		for i in range(1, len(temp_item)):  # splitting up comments to their own row
-			for j, elm in enumerate(temp_item[i]):
-				data_dict[temp_item[0][j]] = elm.replace('\r', '\\r') if type(elm) == str else elm
-			master_list.append(data_dict)
-			data_dict = {}
-	else:  # works for playlist
-		playlist_videos = soup.find_all('div', {'id': 'contents'})[0]
-		videos = playlist_videos.find_all('div', {'id': 'content'})
-		for video in videos:
-			data_dict = {}
-			# get title
-			data_dict['title'] = video.find('a', {'id': 'video-title'})['title']  # was a 'span'
-			# get video url + use id for youtube API
-			video_id = getIDFromLink(video.find('a', {'class': 'yt-simple-endpoint'})['href'], 'v=')
-			data_dict, data_dict['comments'] = youtubeAPI(video_id, api_key, data_dict, commentsOn)
-			master_list.append(data_dict)
-			printProgressBar(videos.index(video) + 1, len(videos), 'Processing Videos:', length=50)
-
-	print('')  # new line for downloaded file
-	driver.close()
-	# put data into a CSV file + download to downloads folder
-	youtube_df = pd.DataFrame(master_list)
-	youtube_df.to_csv(str(Path.home() / "Downloads/") + '/' + title, index=False)
-	print('downloaded', title)
+		# put data into a CSV file + download to downloads folder
+		youtube_df = pd.DataFrame(master_list)
+		youtube_df.to_csv(str(Path.home() / "Downloads/") + '/' + title, index=False)
+		print('downloaded', title)
+		
