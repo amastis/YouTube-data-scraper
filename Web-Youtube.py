346596f779d5a569e2dbc7eeddc9a74ac1ec0e9d
amastis <37512516+amastis@users.noreply.github.com>
2025-03-23 02:23:02 -0400

    Update Web-Youtube.py (playlist option)

--- Web-Youtube.py
+++ Web-Youtube.py
@@ -1,5 +1,6 @@
 ''' generate csv files of user defined youtube links '''
 import os
+import json
 from time import sleep
 from pathlib import Path
 from math import ceil
@@ -10,6 +11,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import TimeoutException
 #from webdriver_manager.chrome import ChromeDriverManager # pre chrome v.115
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -26,22 +28,36 @@ def get_channel_link(user_id: str, split_link: List[str], new_channel: bool) -> 
     else:
         return f'https://www.youtube.com/{split_link[3]}/{user_id}/'
 
+def get_channel_id(driver) -> str:
+    for item in driver.find_elements(By.TAG_NAME, 'script'):
+        text_elm: str = item.get_attribute("textContent")
+        if text_elm and 'ytInitialData' in text_elm:
+            ytInitialData = json.loads(text_elm[20:-1])
+            return ytInitialData['contents']['twoColumnBrowseResultsRenderer']['tabs'][0]['tabRenderer']['endpoint']['browseEndpoint']['browseId']
+
+def scroll(driver) -> None:
+    driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+
 def scroll_selenium(driver, scroll_amnt: int, description: str) -> None:
     ''' scrolls to the bottom of the page to load all of the videos in '''
     for _ in trange(scroll_amnt, desc=description):  # scroll to bottom of page
-        driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
-        sleep(3)
+        scroll(driver)
+        sleep(15)
 
 def load_and_navigate(driver, channel_link: str, page_type: str, load_amnt: int, num_shorts: int) -> None:
-    if page_type != 'playlist':
+    if page_type != 'playlist' or page_type not in driver.current_url:
         driver.get(f'{channel_link}{page_type}')
         sleep(5) # replace w/ wait function till elements are visible
     # 48 videos load at a time
     scroll_selenium(driver, abs(num_shorts) // load_amnt + 1, f'Navigating {page_type}') # get all of type
 
 def get_content(driver, scraper, channel_link: str, desc_type: str, load_amnt: int, num_others: int, filter_dict: Dict[str, str]) -> Dict[str, Any]:
-    load_and_navigate(driver, channel_link, desc_type, load_amnt, num_others)
-    
+    try:
+        load_and_navigate(driver, channel_link, desc_type, load_amnt, num_others)
+    except:
+        print(f'Failed to load {desc_type}')
+        return {}
+
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     content = soup.find_all('a', filter_dict, href=True)
     if len(content) == 0:
@@ -71,7 +87,7 @@ def main() -> None:
     driver.find_elements(By.TAG_NAME, 'button')[6].click() # currently button is in location 6/7
     sleep(5) # wait for redirect
     '''
-    
+
     for yt_link in all_links: # for multiple links inserted
         print(yt_link, '')
         scroll_amnt: int = 5  # arbitrary number for homepage/search -- get user number to override?
@@ -79,15 +95,16 @@ def main() -> None:
 
         title: str = ''
         title_id: str = 'video-title'  # default is for channels
-        user_id: str = ''
+        desc_type: str = '' # switch between whether channel or playlist
+        # vars for channels
         channel_link: str = ''
-        desc_type: str = '' # switch between channel or playlist
-        split_link: List[str] = []
-        num_videos: int = 0 # for channels
-        videos_flag, shorts_flag, live_flag = False, False, False # for channels
+        channel_id: str = ''
+        num_videos: int = 0
+        videos_flag, shorts_flag, live_flag = False, False, False
+
         if any(x in yt_link for x in ['/user/', '/channel/', '/c/', '/@']):  # Channel
-            split_link = yt_link.split('/')
-            user_id = split_link[-2] if 'videos' in yt_link else split_link[-1]
+            split_link: List[str] = yt_link.split('/')
+            user_id: str = split_link[-2] if 'videos' in yt_link else split_link[-1]
             user_or_channel: str = f'forUsername={user_id}' if '/user/' in yt_link else 'id='
             if '/user/' not in yt_link:
                 driver.get(yt_link)
@@ -102,7 +119,9 @@ def main() -> None:
             if '/@' in yt_link:
                 title_id = 'video-title-link'
             sleep(5) # TODO remove this for wait element
-            title = driver.find_elements(By.ID, 'channel-name')[1].text + '.csv'
+            if data_opt['playOn']:
+                channel_id = get_channel_id(driver)
+            title = driver.find_elements(By.CLASS_NAME, 'dynamic-text-view-model-wiz__h1')[0].text + '.csv'
             # types of videos on the channel
             for item in driver.find_element(By.ID, 'tabsContent').find_elements(By.TAG_NAME, 'yt-tab-shape'):
                 item_text = item.text.lower()
@@ -137,6 +156,8 @@ def main() -> None:
             continue
 
         final_list: List[Dict[str, Any]] = []
+        playlist_info: List[Dict[str, Any]] = []
+
         if not single_video:  # channel, homepage, and search + playlists
             if videos_flag: # desc_type for videos and playlists
                 final_list.extend(get_content(driver, scraper, channel_link, desc_type, 30, num_videos, {'id': title_id})) # TODO change load_amnt 
@@ -147,9 +168,11 @@ def main() -> None:
                 num_others = num_videos - len(final_list) + 1 # 1 for extra row headers
                 final_list.extend(get_content(driver, scraper, channel_link, 'streams', 24, num_others, {'id': title_id}))
 
+            if data_opt['playOn']:
+                playlist_info = scraper.playlist_videos_list(channel_id) # pandas merge before save 
         else:  # single video
             soup = BeautifulSoup(driver.page_source, 'html.parser')
-            title = soup.find('meta', {'name': 'title'})['content'] + '.csv' # TODO title is no longer shown in soup file
+            title = f"{soup.find('meta', {'name': 'title'})['content']}.csv" # TODO title is not shown in soup
             final_list = scraper.video_data([soup], 'Video')
             # make comments their own items in table
             if data_opt['cmtOn'] and 'comments' in final_list[0] and len(final_list[0]['comments']) > 1: # checking there are comments
@@ -159,6 +182,9 @@ def main() -> None:
 
         # put data into a CSV file + download to downloads folder
         youtube_df = pd.DataFrame(final_list)
+        if playlist_info: # adding playlist_info before saving to csv
+            df2 = pd.DataFrame(playlist_info)
+            youtube_df = pd.merge(youtube_df, df2, on='video_url', how='left')
         youtube_df.to_csv(str(Path.home() / "Downloads" / title.replace('/', '')), index=False)
         print(f'\nDownloaded: {title}')
     driver.close()
